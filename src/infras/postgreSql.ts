9c600e1ea5fb029e2756c9992fd9d129c154bcdf
arif <wahyudiansyah@gmail.com>
2025-01-10 19:52:25 +0700

    add env file

--- src/infras/postgreSql.ts
+++ src/infras/postgreSql.ts
@@ -1,21 +1,29 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Access environment variables
+const dbHost = process.env['DB.HOST'];
+const dbName = process.env['DB.NAME'];
+const dbUserName = process.env['DB.USER'];
+const dbPassword = process.env['DB.PASSWORD'];
+const dbLogging   = process.env['DB.LOGGING'];
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: dbHost,
     port: 5432,
-    username: 'postgres', // replace with your PostgreSQL username
-    password: 'postgres', // replace with your PostgreSQL password
-    database: 'library',
+    username: dbUserName,
+    password: dbPassword,
+    database: dbName,
     entities: ['dist/domain/**/*.js'],
     migrations: ['dist/migrations/*.js'],
     migrationsTableName: 'migrations',
-    logging: true,
+    logging: dbLogging === 'true',
 });
 
-export const Connection =  async () => {
-    await AppDataSource.initialize();
-    return AppDataSource
+export const Connection = async (): Promise<DataSource> => {
+    return await AppDataSource.initialize();
 };
 
 export default AppDataSource;
